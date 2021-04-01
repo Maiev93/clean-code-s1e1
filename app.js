0bab5832367a6d4ff4132742e0dbbe756bfb89d0
Maiev <elena.kalichkina.rho@gmail.com>
2021-04-02 04:45:03 +0700

    refactor: add BEM classes, rules 3.1-3.4, rule 2 advanced

--- app.js
+++ app.js
@@ -9,8 +9,9 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var addButton=document.querySelector('.add__button');//first button
+console.log(addButton);
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
